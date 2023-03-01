69945b58c75fb8c6e1e894efd92be34cd4b96df8
Trent1900 <lc20200212@gmail.com>
2023-03-01 11:42:32 +1100

    fix : add a tmp to store db to enable post request

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("/tem/test_db.json");
 const middleware = jsonServer.defaults();
 
 const PORT = process.env.JSON_SERVER_PORT;
